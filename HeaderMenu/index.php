fa51a36b605309c9dfbe21292fd5b950ff20830a
ViniciusbcMarques <morenopagode13@gmail.com>
2025-10-20 14:12:10 -0300

    php

---
+++ HeaderMenu/index.php
@@ -0,0 +1,14 @@
+
+<?php
+    include "./assets/includes/doctype.php";
+?>
+<head>
+<title>Pagina Principal</title>
+<!--<link rel="stylesheet" href="../style/style_header.css";-->
+</head>
+<body>
+<?php
+include "./assets/includes/header.php";
+include "./assets/includes/navigationMenu.php";
+?>
+</body>
