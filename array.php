9c813efeb95b60c23c2953b5782fb3914b36feb1
Thg-Antunes <thg.inf3@gmail.com>
2025-11-03 11:30:30 -0300

    adeswffg

--- array.php
+++ array.php
@@ -1,1 +1,22 @@
 <?php
+    $mes= array();
+    $mes=[
+        'Janeiro', 'Fevereiro','Março',
+        'Abril', 'Maio','Junho',
+        'Julho', 'Agosto','Setembro',
+        'Outubro','Novembro', 'Dezembro'
+        ];
+    //IMPRIME AS CHAVES E O VALOR
+    echo 'CHAVE E VALOR <br>';
+    foreach ($mes as $key => $value) {
+        echo $key . ' => ' . $value . '<br>';
+    }
+    echo '<hr>';
+    echo 'VAR_DUMP';
+    var_dump($mes);
+    echo '<hr>';
+    echo 'Posiçao 1 do array MÊS <br>';
+    echo $mes[1];
+    echo '<hr>';
+
+    
